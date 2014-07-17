a6d3cf62b853bdbe4ab876ed981f15c17bd6524f
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2014-07-17 09:25:04 -0400

    fixing title position of no photos found view

--- NFTImagePickerController/NFTNoPhotosFoundCell.m
+++ NFTImagePickerController/NFTNoPhotosFoundCell.m
@@ -59,7 +59,7 @@
     [super layoutSubviews];
 
     CGFloat x = 0, w = CGRectGetWidth(self.bounds), h = 22;
-    CGFloat y = (CGRectGetWidth(self.contentView.bounds) / 2) - h;
+    CGFloat y = (CGRectGetHeight(self.contentView.bounds) / 2) - (h * 2);
     self.titleLabel.frame = CGRectMake(x, y, w, h);
 
     y += h + 10;
